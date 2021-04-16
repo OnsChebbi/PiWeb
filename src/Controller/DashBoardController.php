8d533af6c7b068bd879d8af6b0e24b04c37ef98e
oussmasboui <49285853+oussmasboui@users.noreply.github.com>
2021-04-17 02:51:01 +1200

    files upload

---
+++ src/Controller/DashBoardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashBoardController extends AbstractController
+{
+    /**
+     * @Route("/dash/board", name="dash_board")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'DashBoardController',
+        ]);
+    }
+}
